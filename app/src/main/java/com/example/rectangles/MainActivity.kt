566764a85277ab02d8077ac3f794a927bb010851
Alex <mike123777mike@gmail.com>
2023-11-22 09:09:07 +0300

    rk-editing

--- app/src/main/java/com/example/rectangles/MainActivity.kt
+++ app/src/main/java/com/example/rectangles/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -56,28 +59,42 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(n: Int, mutNum: MutableState<Int>) {
     val number = remember { mutNum }
-    var index = 0
+
+    var list: MutableList<Int> = ArrayList()
+    for (i in 0 until n) {
+        list.add(i)
+    }
+
+    val q = remember { mutableStateOf(-1) }
     val y_max = number.value / n + if(number.value % n != 0) 1 else 0
     val height = LocalConfiguration.current.screenHeightDp.dp
     Column {
-        Column(modifier = Modifier
-            .height(height - 75.dp)
-            .verticalScroll(rememberScrollState())) {
-            for (y in 1..y_max)
-                Row(modifier = Modifier) {
-                    for (x in 1..(if (number.value % n == 0 || y != y_max) n else number.value % n)) {
-                        Box(
-                            modifier = Modifier
-                                .background(if (index % 2 == 0) Color.Red else Color.Blue)
-                                .padding(vertical = 15.dp)
-                                .fillMaxWidth(1f / (n + 1 - x)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("$index", fontSize = 20.sp, color = Color.White, fontWeight = FontWeight.SemiBold)
+        LazyColumn(modifier = Modifier
+            .height(height - 75.dp)) {
+            itemsIndexed(list) { index, item ->
+                for (y in 1..y_max)
+                    Row(modifier = Modifier) {
+                        for (x in 1..(if (number.value % n == 0 || y != y_max) n else number.value % n)) {
+                            var color: Color
+                            if (q.value == index)
+                                color = Color.Green
+                            else if (index % 2 == 0)
+                                color = Color.Red
+                            else
+                                color = Color.Blue
+                            Box(
+                                modifier = Modifier
+                                    .background(color)
+                                    .padding(vertical = 15.dp)
+                                    .fillMaxWidth(1f / (n + 1 - x)).clickable { q.value = index },
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text("$index", fontSize = 20.sp, color = Color.White, fontWeight = FontWeight.SemiBold)
+                            }
                         }
-                        index += 1
                     }
-                }
+            }
+
         }
         Button(onClick = { number.value++ }, shape = RoundedCornerShape(0.dp), modifier = Modifier
             .height(75.dp)
